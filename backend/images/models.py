872bfbd7cae5189e514ae2a115c5b0ad3dce6ab1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-07 09:40:01 -0500

    Comment + Like Models

--- backend/images/models.py
+++ backend/images/models.py
@@ -1,20 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Image(models.Model):
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
 
     """ Image Model """
 
 
-class Like(models.Model):
+class Like(TimeStampedModel):
 
     """ Like Model """
 
+    image = models.ForeignKey(
+        Image, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='comments', on_delete=models.CASCADE)
 
-class Comment(models.Model):
+
+class Comment(TimeStampedModel):
 
     """ Comment Model """
 
     message = models.TextField()
     image = models.ForeignKey(
         Image, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, related_name='comments', on_delete=models.CASCADE)
