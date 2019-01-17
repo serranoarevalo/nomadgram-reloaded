4be445072b4959170990d72ed99650a653554ba7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-17 18:18:34 -0500

    Header Username

--- frontend/src/Styles/GlobalStyles.js
+++ frontend/src/Styles/GlobalStyles.js
@@ -12,6 +12,9 @@ const GlobalStyles = createGlobalStyle`
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size:14px;
     }
+    svg{
+        fill:#262626;
+    }
 `;
 
 export default GlobalStyles;
--- frontend/src/sharedQueries.js
+++ frontend/src/sharedQueries.js
@@ -5,3 +5,13 @@ export const LOG_USER_IN = gql`
     logUserIn(token: $token) @client
   }
 `;
+
+export const ME = gql`
+  {
+    me {
+      user {
+        username
+      }
+    }
+  }
+`;
