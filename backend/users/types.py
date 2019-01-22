d5a7aafb488733c8ed4f8337a871c2d579f847ee
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-22 16:10:21 -0500

    is_self

--- backend/users/types.py
+++ backend/users/types.py
@@ -11,10 +11,18 @@ class ProfileType(DjangoObjectType):
     followers_count = graphene.Int(source='following_count')
     post_count = graphene.Int(source='post_count')
     is_following = graphene.Boolean()
+    is_self = graphene.Boolean()
 
     def resolve_is_following(self, info):
         user = info.context.user
         if self in user.profile.following.all():
+            return True
+        else:
+            return False
+
+    def resolve_is_self(self, info):
+        user = info.context.user
+        if self.user.id == user.id:
             return True
         else:
             return False
