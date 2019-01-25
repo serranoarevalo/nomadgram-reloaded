899de4d76bc38b7f825e622f298afb5d9f7772e4
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-01-24 22:41:10 -0500

    AppLoading

--- app/App.js
+++ app/App.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Font } from "expo";
+import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+const RedText = styled.Text`
+  color: red;
+`;
 
 export default class App extends React.Component {
+  state = {
+    isLoaded: false
+  };
+
+  loadAssetsAsync = async () => {
+    Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  handleLoadingFinish = () => {
+    this.setState({
+      isLoaded: true
+    });
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { isLoaded } = this.state;
+    if (isLoaded) {
+      return (
+        <AppLoading
+          onError={this.handleLoadingError}
+          onFinish={this.handleLoadingFinish}
+          startAsync={this.loadAssetsAsync}
+        />
+      );
+    } else {
+      return (
+        <Container>
+          <RedText>lalal</RedText>
+        </Container>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
